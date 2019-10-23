3675b66356bf6fa2830cf38d678eefac18643035
syyms <symacademy@gmail.com>
2019-10-23 17:12:57 +0200

    create player function add

--- pendu/player_list.py
+++ pendu/player_list.py
@@ -1,8 +1,20 @@
 # config: utf-8
 
 def print_player_list():
+    print("\t\t\n\n===  Liste des joueurs  ===\n")
     players = [{"nom": "simon", "score": 22}, {"nom": "mohammed", "score": 23}, {"nom": "romane", "score": 1000}]
     players.append({"nom":"zozo", "score":0})
 
     for i in players:
-        print(i["nom"], i["score"])
+        print("[", i["nom"], "]", "\tscore ==>\t", i["score"])
+
+    print("_______________________________________________________")
+
+def create_player():    newplayer = input("veuiller saisir un nom")
+   for i in players:
+       if (newplayer in i["nom"]):
+           print ("joueur existant")
+           create_player()
+       else:
+           players.append({"nom":nom, "score":0})
+           print("bienvenue")
